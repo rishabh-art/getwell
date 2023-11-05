a3d8efdd5a749990866e7f79cc7061a05cf69688
palrishabh <58916474+rishabh-art@users.noreply.github.com>
2023-11-05 21:22:54 +0530

    Update script.js
    Mobile viewport

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const scrollDownButton = document.getElementById('scroll-down-button');
+    const menuButton = document.getElementById('menu-button');
+    const navbar = document.querySelector('.navbar');
 
     scrollDownButton.addEventListener('click', function () {
         document.body.style.overflow = 'auto';
         document.querySelector('.content').scrollIntoView({ behavior: 'smooth' });
+    });
+
+    menuButton.addEventListener('click', function () {
+        navbar.classList.toggle('active');
     });
 
     setTimeout(function () {
